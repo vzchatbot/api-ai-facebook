150d0134886e5a7c5bd5d2032dcdc5887e8d30ba
vzchatbot <vzchatbot@hotmail.com>
2016-12-02 20:58:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -95,7 +95,7 @@ function processEvent(event) {
 	    
 	var ReqSenderID = event.sender.id.toString();
         var ReqRecipientID = event.recipient.id.toString();
-	   
+	  var ReqMessageText = text;  
 	    if(event.timestamp)
 	    {
 		var ReqTimeStamp = event.timestamp.toString();
@@ -107,13 +107,7 @@ function processEvent(event) {
 			var ReqMessageID = event.message.mid.toString();
 		    }
 	    }
-	    if(event.message)
-	    {
-		     if(event.message.text)
-		    {
-			var ReqMessageText = event.message.text.toString();   
-		    }
-	    }
+	  
 	/*    
         console.log("SSSReqSenderID :" + JSON.stringify(ReqSenderID));
         console.log("SSSReqRecipientID :" + JSON.stringify(ReqRecipientID));
@@ -159,7 +153,7 @@ function processEvent(event) {
                 console.log('action : - '+ action );
                 console.log('intent : - '+ intent );	
 		    
-console.log("||" + JSON.stringify(ReqSenderID) + "||" + JSON.stringify(ReqRecipientID) +"||"+ JSON.stringify(ReqTimeStamp) + "||" + JSON.stringify(ReqMessageID) + "|| "+ JSON.stringify(ReqMessageText)+ "||"  + JSON.stringify(action) + "||"+  JSON.stringify(intent)+ "|| Undefined");	    
+console.log("ProcessEvent," + JSON.stringify(ReqSenderID) + ", + JSON.stringify(ReqRecipientID) +","+ JSON.stringify(ReqTimeStamp) + "," + JSON.stringify(ReqMessageID) + ", "+ JSON.stringify(ReqMessageText)+ ","  + JSON.stringify(action) + ","+  JSON.stringify(intent) + ",Undefined");	    
  
 		    
                 // see if the intent is not finished play the prompt of API.ai or fall back messages
@@ -436,8 +430,8 @@ app.post('/webhook/', function (req, res)  {
 					  var TimeStamp = event.timestamp;
 					  var MessageID = event.message.mid;
 					  var MessageText = event.message.text;
-console.log("||" + JSON.stringify(SenderID) + "||" + JSON.stringify(RecipientID) +"||"+ JSON.stringify(TimeStamp) +
-	    "||" + JSON.stringify(MessageID) + "|| " + JSON.stringify(MessageText)+ "|| Undefined || Undefined ||" + JSON.stringify(data));
+console.log("," + JSON.stringify(SenderID) + "," + JSON.stringify(RecipientID) +","+ JSON.stringify(TimeStamp) +
+	    "," + JSON.stringify(MessageID) + "," + JSON.stringify(MessageText)+ ", Undefined , Undefined ," + JSON.stringify(data));
 
                        		 } 
                         if (event.message && !event.message.is_echo ||
