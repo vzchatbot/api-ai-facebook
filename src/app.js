d6b436789e3f9501829797fccb47595e7592d406
vzchatbot <vzchatbot@hotmail.com>
2016-12-18 23:29:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -669,8 +669,9 @@ function getVzProfile(apireq,callback) {
         }		
     };
 console.log("args=" + JSON.stringify(args));
-    request.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
+    request.post("https://www.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
         function (error, response, body) {
+	    console.log("statusCode---- " + response.statusCode);
             if (!error && response.statusCode == 200) {             
                 console.log("body " + body);
                 callback(body);
@@ -751,7 +752,7 @@ console.log("args=" + JSON.stringify(args));
     request.post("https://www.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
         function (error, response, body) {
             if (!error && response.statusCode == 200) { 
-		       console.log("statusCode " + response.statusCode);
+		       
                 console.log("body " + body);
                 callback(body);
             }
