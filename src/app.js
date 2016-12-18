42e5604e0a46b109373a5869ecdfde1f744ab212
vzchatbot <vzchatbot@hotmail.com>
2016-12-18 23:58:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -750,7 +750,7 @@ console.log('Inside showOutageCallback');
         }		
     };
 console.log("args=" + JSON.stringify(args));
-    request.post("https://www.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
+    request.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/restapi.ashx", args,
         function (error, response, body) {
             if (!error && response.statusCode == 200) { 
 		       
