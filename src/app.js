f92a275a68bb3e611a8a3fde53ba14b00209ddd4
vzchatbot <vzchatbot@hotmail.com>
2016-12-26 16:15:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -840,7 +840,7 @@ console.log("args=" + JSON.stringify(args));
 	  	 var respobj ={"facebook":{"attachment":{"type":"template","payload":
                       {"template_type":"button","text":"Are you sure to cancel this appointment ?","buttons":[
 		       {"type":"postback","title":"Cancel","payload":"Open Tickets"},
-			{"type":"postback","title":"Confirm","payload":"Want to cancel, statecode , cancel status canConfirmed"}
+			{"type":"postback","title":"Confirm","payload":"Want to cancel"+strCancelTicketNumber+ " statecode "+strTCStateCode+ " cancel status canConfirmed"}
 			]}}}};
 			     
 			     /* var respobj ={"facebook":{"attachment":{"type":"template","payload":
