9f8c7ee27a195520b34dee050c485df8d3089aac
vzchatbot <vzchatbot@hotmail.com>
2017-02-02 16:31:17 +0530

    Update log.js

--- src/log.js
+++ src/log.js
@@ -15,35 +15,5 @@ var logger = new (winston.Logger)({
 module.exports = logger;
 
 /*
-{
-  "id": "d91a367a-1c22-4cac-8840-599450b92865",
-  "name": "Program",
-  "isOverridable": true,
-  "entries": [
-    {
-      "value": "channel",
-      "synonyms": [
-        "HBO",
-        "AXN",
-        "STAR",
-        "channel"
-      ]
-    },
-    {
-      "value": "Shows",
-      "synonyms": [
-        "Shows"
-      ]
-    },
-    {
-      "value": "programname",
-      "synonyms": [
-        "programname"
-      ]
-    }
-  ],
-  "isEnum": false,
-  "automatedExpansion": false
-}
-
+[{"value":"1700 CTI News","synonyms":["1700 CTI News"]},{"value":" 1st Time She Came","synonyms":["1st Time She Came"]},{"value":" 2017 CFP National Championship","synonyms":["2017 CFP National Championship"]},{"value":" 21st Century","synonyms":["21st Century"]},{"value":" 300 Rise of an Empire","synonyms":["300 Rise of an Empire"]},{"value":" 40 Days and 40 Nights","synonyms":["40 Days and 40 Nights"]},{"value":" 80 cm","synonyms":["80 cm"]},{"value":" A Bridge Too Far","synonyms":["A Bridge Too Far"]},{"value":" A Craftsman's Legacy","synonyms":["A Craftsman's Legacy"]},{"value":" A Different World","synonyms":["A Different World"]},{"value":" A Dormir con�","synonyms":["A Dormir con..."]},{"value":" A Dream Home Planning","synonyms":["A Dream Home Planning"]}]
 */
