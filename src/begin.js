3c9aeba15809fe223478f0aa398c1e62f8ce823f
JosephPravin <josephpravinn@gmail.com>
2017-02-22 17:54:48 +0530

    Update begin.js

--- src/begin.js
+++ src/begin.js
@@ -2,8 +2,8 @@ var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
 // Set up Conversation service wrapper.
 var conversation = new ConversationV1({
-  username: 'USERNAME', // replace with username from service key
-  password: 'PASSWORD', // replace with password from service key
+  username: '31be4934-c02e-441a-96e6-d639b4ab69a8', // replace with username from service key
+  password: 'Q2hapKhopVRj', // replace with password from service key
   path: { workspace_id: 'b2d3a074-4d46-4b95-b902-f70d0000fdc6' }, // replace with workspace ID
   version_date: '2016-07-11'
 });
