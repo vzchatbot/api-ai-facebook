6588046f0505473bf531d1040ef11111c14a17b3
JosephPravin <josephpravinn@gmail.com>
2017-02-23 15:19:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -125,7 +125,7 @@ function processEvent(event) {
 	    
 	    conversation.message({
         workspace_id: 'b2d3a074-4d46-4b95-b902-f70d0000fdc6',
-        input: { 'text': text }
+        input: { 'text': 'Hi' }
     }, function (err, response) {
         if (err) {
 		console.log('WATSON ERROR');
