7412d7144e2ee3e1c1b7c44cc64e90036ecc8e56
vzchatbot <vzchatbot@hotmail.com>
2017-02-23 13:54:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -63,6 +63,13 @@ var logger = log4js.getLogger("botws");
 var Errlogger = log4js.getLogger('errorlog');
 var ChatHistoryLog = log4js.getLogger('Debug');
 
+ var conversation = watson.conversation({
+    username: '31be4934-c02e-441a-96e6-d639b4ab69a8',
+    password: 'Q2hapKhopVRj',
+    version: 'v1',
+    version_date: '2016-09-20'
+});    
+
 //=========================================================
 /*
 {
@@ -172,7 +179,21 @@ function processEvent(event) {
                 console.log('action : - '+ action );
                 console.log('intent : - '+ intent );	
 */
-	    
+    conversation.message({
+        workspace_id: 'b2d3a074-4d46-4b95-b902-f70d0000fdc6',
+        input: { 'text': text }
+    }, function (err, response) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+	
+            console.log('I got a response. Let me check');
+            if (response.output.text.length != 0) {
+                console.log('Watson says:' + response.output.text[0]);                
+            }
+        }
+    });
 	    
 	    
 	    
