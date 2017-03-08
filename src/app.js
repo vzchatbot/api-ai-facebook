68fe0e909a7e79255f9f0113a7a971c19d6474f9
JosephPravin <josephpravinn@gmail.com>
2017-03-08 18:23:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1439,7 +1439,7 @@ function PgmListing(usersession, msg)
 function alertResp(usersession, msg)
 	{
 		console.log("inside ALERT REPLY");
-		sendFBMessage(usersession, {"facebook":msg});
+		sendFBMessage(usersession, {facebook:msg});
 		
 	}
 function defaultReply(usersession, msg)
